02c00a1638855b655eaa5b436060531f97d442a7
Avanish Yadav <avanishydv29@gmail.com>
2026-08-16 18:29:59 +0530

    Update

---
+++ src/operators.java
@@ -0,0 +1,79 @@
+public class operators {
+    public static void main(String[] args){
+
+
+
+//                int solvedThisWeek = 25;
+//                int solvedLastWeek = 15;
+//
+//                int total = solvedThisWeek + solvedLastWeek;
+//                int difference = solvedThisWeek - solvedLastWeek;
+//                int projected = solvedThisWeek * 4;
+//                int average = solvedThisWeek / 7;
+//                int remainder = solvedThisWeek % 7;
+//
+//                System.out.println(total);
+//                System.out.println(difference);
+//                System.out.println(projected);
+//                System.out.println(average);
+//                System.out.println(remainder);
+
+
+//        int currentStreak = 45;
+//        int targetStreak = 50;
+//
+//        System.out.println(currentStreak == targetStreak);
+//        System.out.println(currentStreak != targetStreak);
+//        System.out.println(currentStreak > targetStreak);
+//        System.out.println(currentStreak < targetStreak);
+//        System.out.println(currentStreak >= targetStreak);
+//        System.out.println(currentStreak <= targetStreak);
+
+
+//        boolean completedDSA = true;
+//        boolean completedCore = false;
+//
+//        System.out.println(completedDSA && completedCore);
+//        System.out.println(completedDSA || completedCore);
+//        System.out.println(!completedCore);
+
+//
+//        int ratingPoints = 100;
+//
+//        ratingPoints += 20;  // ratingPoints = ratingPoints + 20
+//        System.out.println(ratingPoints);
+//        ratingPoints -= 10;  // ratingPoints = ratingPoints - 10
+//        System.out.println(ratingPoints);
+//        ratingPoints *= 2;   // ratingPoints = ratingPoints * 2
+//        System.out.println(ratingPoints);
+//        ratingPoints /= 4;   // ratingPoints = ratingPoints / 4
+//        System.out.println(ratingPoints);
+//        ratingPoints %= 30;  // ratingPoints = ratingPoints % 30c
+//        System.out.println(ratingPoints);
+
+//
+//        int activeUsers = 100;
+//
+//        int prefix = ++activeUsers;
+        // pahle activeuser ko inc kar diya phir use kiya
+//        int postfix = activeUsers++;
+        // pahle active user ko use kiya phir inc kiya
+//
+//        System.out.println(prefix);
+//        System.out.println(postfix);
+//        System.out.println(activeUsers);
+
+
+        int x = 6;  // 00000110
+        int y = 3;  // 00000011
+
+        System.out.println(x & y);
+        System.out.println(x | y);
+        System.out.println(x ^ y);
+        System.out.println(~x);
+        System.out.println(x << 1);
+        System.out.println(x >> 1);
+
+    }
+
+}
