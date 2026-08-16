4ad93432f2b416ceffee4c0f9cb0b331b6741c42
Avanish Yadav <avanishydv29@gmail.com>
2026-08-16 21:13:46 +0530

    Update

--- src/loop.java
+++ src/loop.java
@@ -146,7 +146,20 @@ public class loop {
 //            System.out.println("Avanish Yadav");
 //        }
 
-
+//        for (int n = 2; n <= 100; n++) {
+//
+//            int count = 0;
+//
+//            for (int i = 1; i <= n; i++) {
+//                if (n % i == 0) {
+//                    count++;
+//                }
+//            }
+//
+//            if (count == 2) {
+//                System.out.print(n + " ");
+//            }
+//        }
 
 
 
