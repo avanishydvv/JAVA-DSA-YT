19a22ae2e372caf31b43f38ad9ddbd103a765e40
Avanish Yadav <avanishydv29@gmail.com>
2026-08-16 18:20:38 +0530

    Add Java files

---
+++ src/loop.java
@@ -0,0 +1,9 @@
+public class loop {
+    public static void main(String[] args) {
+        int i = 5;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
