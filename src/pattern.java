14f8ed3c8a6dfb068f7d056825511626aed553e1
Avanish Yadav <avanishydv29@gmail.com>
2026-08-19 06:33:00 +0530

    Update

--- src/pattern.java
+++ src/pattern.java
@@ -24,17 +24,96 @@ public class pattern {
 //
 
         // pattern 3
+//        int n=5;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        // Pattern 4
+
+//        int n=5;
+//
+//        for (int row = 1; row <= n; row++) {
+//            // for each row -> spaces, stars
+//
+//            // spaces
+//            for (int col = 1; col <= n-row ; col++) {
+//                System.out.print(" ");
+//            }
+//
+//            // stars
+//            for (int col = 1; col <= n ; col++) {
+//                System.out.print("* ");
+//            }
+//            // move to next row
+//            System.out.println();
+//        }
+
+//
+//        int n=5;
+//
+//        for (int row = 1; row <= n ; row++) {
+//            for (int col = 1; col <= n-row+1 ; col++) {
+//              
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        int n=5;
+//
+//        for (int row = 1; row <= n; row++) {
+//
+//            for (int s = 1; s<= n-row; s++) {
+//                System.out.print(" ");
+//            }
+//
+//            for (int col = 1; col <= 2*row -1  ; col++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+
+//           int n=4;
+//
+//        for (int row = 1; row <= n ; row++) {
+//            for (int s = 1; s <= row-1; s++) {
+//                System.out.print(" ");
+//            }
+//
+//            for (int col = 1; col <= 2*n-row-(row-1) ; col++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//           int n=4;
+//
+//        for (int row = 1; row <= 4 ; row++) {
+//
+//            for (int col = 1; col <= 6 ; col++) {
+//
+//                if(row==1 || row == 4 || col ==1 || col == 6)
+//                    System.out.print("*");
+//                else
+//                    System.out.print(" ");
+//            }
+//            System.out.println();
+//
+//        }
+        
+
+
         int n=5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        for (int row = 1; row <= n ; row++) {
+
+
         }
-
-
-
-
 
 
 
