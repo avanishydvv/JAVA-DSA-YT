9622d5658890ce7d4ecbde86b52ed6406bcc7bf9
Avanish Yadav <avanishydv29@gmail.com>
2026-08-20 19:40:17 +0530

    Update

--- src/methods.java
+++ src/methods.java
@@ -1,7 +1,26 @@
 public class methods {
+    // declaration/definition
+    static void print2kaTable()
+    {
+        for (int i = 1; i <= 10 ; i++) {
+            int ans=2*i;
+            System.out.println(ans);
+        }
+    }
+
+    static void sum(int x,int y)
+    {
+        System.out.println("sum:"+ (x+y) );
+    }
     public static void main(String[] args) {
 
         /* Methods in java  */
+//        System.out.println("hi");
+//        print2kaTable();
+//        System.out.println("bye");
+//          sum(5,10);
+
+
 
 
 
