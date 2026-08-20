beea739e23d4e00a9a4857fedfcc9535927afe04
Avanish Yadav <avanishydv29@gmail.com>
2026-08-20 21:21:46 +0530

    LEC 12 Methods in JAVA is completed

--- src/methods.java
+++ src/methods.java
@@ -24,6 +24,16 @@ public class methods {
         int sum=a+b;
         return sum;
     }
+    static int add(int a,int b,int c)
+    {
+        int sum=a+b+c;
+        return sum;
+    }
+    static void solve(int num){
+        System.out.println("inside solve:"+num);
+        num=num+10;
+        System.out.println("inside solve: "+ num);
+    }
     public static void main(String[] args) {
 
         /* Methods in java  */
@@ -33,10 +43,15 @@ public class methods {
 //          sum(5,10);
 //        multiply(5,6);
 //        System.out.println(add(2,3));
-        int result = add(2,5);
-        System.out.println(result);
-
-
+//        int result = add(2,5);
+//        System.out.println(result);
+//
+//        int ans = add(2,5,6);
+//        System.out.println(ans);
+        int num = 5;
+        System.out.println("inside main: " + num);
+        solve(num);
+        System.out.println("inside main: " + num);
 
 
 
