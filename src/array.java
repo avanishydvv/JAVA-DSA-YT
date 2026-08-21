38b10a4aaa3928b60dd14837c29e4b34d75380d1
Avanish Yadav <avanishydv29@gmail.com>
2026-08-21 22:44:31 +0530

    Update

--- src/array.java
+++ src/array.java
@@ -89,9 +89,60 @@ public class array {
 
 
 
+            // 2d array
+
+//            int arr[][];
+//            arr = new int[3][4];
+//            int[][] brr ={
+//                    {1,3},
+//                    {2,3},
+//                    {4,5},
+//                    {7,8}
+//            };
+////        System.out.println(brr[3][0]);
+//        int rowLength= brr.length;
+//        int colLength= brr[0].length;
+//
+//
+//        for (int row = 0; row <= rowLength -1 ; row++) {
+//            for (int col = 0; col <= colLength -1; col++) {
+//                System.out.print(brr[row][col]+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+//        int arr[][];
+//        arr = new int[3][4];
+//        int[][] brr ={
+//                {1,3},
+//                {2,3,4,6},
+//                {4,5,42,5,8,3,4},
+//                {7,8,4}
+//        };
+////        System.out.println(brr[3][0]);
+//        int rowLength= brr.length;
+//
+//
+//        for (int row = 0; row <= rowLength -1 ; row++) {
+//            int colLength= brr[row].length;
+//
+//            for (int col = 0; col <= colLength -1; col++) {
+//                System.out.print(brr[row][col]+" ");
+//            }
+//            System.out.println();
+//        }
 
 
 
 
-    }
-}
+
+
+
+
+
+
+
+
+
+}}
