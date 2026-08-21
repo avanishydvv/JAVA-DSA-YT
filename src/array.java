54b0919e67a010f16347c1f70d5dd0202ec838e8
Avanish Yadav <avanishydv29@gmail.com>
2026-08-21 22:16:33 +0530

    Update

--- src/array.java
+++ src/array.java
@@ -1,19 +1,79 @@
+import java.util.Scanner;
+
 public class array {
     public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+//        int arr[]=new int[5];
+//        int n = arr.length;
+//
+//        for (int i = 0; i <= n-1; i++) {
+//            System.out.println("Provide input for index"+i);
+//            arr[i] =sc.nextInt();
+//        }
+//
+//        // print
+//        System.out.println("Your array is: ");
+//        for (int  val :arr)
+//        {
+//            System.out.println(val);
+//        }
+
+
 //        int arr[];
 //        int []arr;
 //        arr = new int[3];
 //        int arr[]={2,3,4,5,6}
+//
+//        int arr[];
+//        arr = new int[5];
+//        int brr[]={10,20,30,40,50};
+//        int n=brr.length;
+//        // for each loop
+//        for(int val : brr)
+//        {
+//            System.out.println(val);
+//        }
+//          for loop
+//        for (int index = 0; index <= n-1; index++) {
+//            System.out.println(brr[index]);
+//        }
+//        System.out.println("Value at 0 index" + brr[0]);
+//        System.out.println("Value at 1 index" + brr[1]);
+//        System.out.println("Value at 2 index" + brr[2]);
 
+//        int arr[]={1,2,3,4,5,};
+//        int n= arr.length;
+//        for (int i = 0; i <= n-1; i++) {
+//            System.out.println(arr[i]);
+//        }
 
+//        int arr[]={10,20,30,40,50};
+//        int n= arr.length;
+//        int sum=0;
+//        for (int i = 0; i <= n-1; i++) {
+//            sum=sum+arr[i];
+//        }
+//        System.out.println("The sum of element of array is :"+ sum);
 
+//        int arr[]={2,3,10,20};
+//        int n= arr.length;
+//        int mul=1;
+//        for (int i = 0; i <= n-1; i++) {
+//            mul=mul*arr[i];
+//        }
+//        System.out.println("The multiplication  of element of array is :"+ mul);
 
-
-
-
-
-
-
+//        int arr[]={2,7,4,80,6,8,8858,1,2,40,5};
+//        int n=arr.length;
+//        int max = arr[0];
+//        for (int i = 0; i <= n-1 ; i++) {
+//            if(arr[i]>max)
+//            {
+//                max=arr[i];
+//            }
+//        }
+//        System.out.println("The max value in this array is :"+ max);
 
 
 
