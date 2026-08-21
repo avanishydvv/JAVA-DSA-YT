457a2991e1e311803a1b821e96a047f1ceeabec1
Avanish Yadav <avanishydv29@gmail.com>
2026-08-21 21:24:03 +0530

    Update

---
+++ src/array.java
@@ -0,0 +1,21 @@
+public class array {
+    public static void main(String[] args) {
+//        int arr[];
+//        int []arr;
+//        arr = new int[3];
+//        int arr[]={2,3,4,5,6}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
