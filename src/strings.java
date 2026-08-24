ca95176e21530e6ec2519a86cc9c8235aa265910
Avanish Yadav <avanishydv29@gmail.com>
2026-08-24 19:22:10 +0530

    Update

--- src/strings.java
+++ src/strings.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class strings {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // strings
+        // strings are immutable
+//        String str="Atish";
+//
+//        String atr=new String("Love");
+//        System.out.println(str+" "+ atr);
+//        System.out.println(str.length());
+//        System.out.println(str.charAt(4));
+//        System.out.println(str.toUpperCase());
+
+//        String name1="love";
+//        String name2="love";
+//
+//        if(name1 == name2)
+//        {
+//            System.out.println("Strings are equal");
+//        }
+//        else {
+//            System.out.println("Strings are not equal");
+//        }
+
+//        String str=sc.nextLine();
+//        System.out.println("Value: "+ str);
+//
+//        String str2=sc.next();
+//        System.out.println("Value: "+ str2);
+
+        String str="babbar";
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        String name="BABBAR";
+        System.out.println(str.equals(name));
+        System.out.println(str.equalsIgnoreCase(name));
+        String trim="   Avanish  ";
+        System.out.println(trim.length());
+        trim=trim.trim();
+        System.out.println(trim.length());
+
+
+
+
+
 
     }
 }
