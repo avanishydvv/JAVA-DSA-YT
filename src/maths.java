ade7446d5fb242fb3de8ea2533d41efa422bce62
Avanish Yadav <avanishydv29@gmail.com>
2026-08-26 18:15:54 +0530

    Update

--- src/maths.java
+++ src/maths.java
@@ -1,5 +1,44 @@
 public class maths {
     public static void main(String[] args) {
 
+
+        int n=567383;
+        System.out.println(n%10);
+        n=n/10;
+        System.out.println(n%10);
+        n=n/10;
+        System.out.println(n%10);
+        n=n/10;
+        System.out.println(n%10);
+        n=n/10;
+        System.out.println(n%10);
+        n=n/10;
+        System.out.println(n%10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
