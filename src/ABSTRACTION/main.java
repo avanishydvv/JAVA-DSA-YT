86c46977c78338e3cbb06ff6cc8a4f591ff86ca6
Avanish Yadav <avanishydv29@gmail.com>
2026-09-26 10:43:58 +0530

    Update

--- src/ABSTRACTION/main.java
+++ src/ABSTRACTION/main.java
@@ -1,46 +1,83 @@
-package ABSTRACTION;
+//package ABSTRACTION;
+//
+//abstract class Bird{
+//    abstract void fly();
+//
+//    abstract void eat();
+//}
+//
+//class sparrow extends Bird {
+//    @Override
+//    void fly() {
+//        System.out.println("Sparrow is flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Sparrow is eating");
+//    }
+//}
+//
+//class Crow extends Bird {
+//    @Override
+//    void fly() {
+//        System.out.println("Crow is flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Crow is eating alag way se");
+//    }
+//}
+//
+//
+//
+//public class main {
+//    public static void doBirdStuff(Bird b){
+//        b.eat();
+//        b.fly();
+//    }
+//    public static void main(String[] args) {
+//       doBirdStuff(new sparrow());
+//       doBirdStuff(new Crow());
+//    }
+//}
 
-abstract class Bird{
-    abstract void fly();
 
-    abstract void eat();
-}
 
-class sparrow extends Bird {
-    @Override
-    void fly() {
-        System.out.println("Sparrow is flying");
-    }
-
-    @Override
-    void eat() {
-        System.out.println("Sparrow is eating");
-    }
-}
-
-class Crow extends Bird {
-    @Override
-    void fly() {
-        System.out.println("Crow is flying");
-    }
-
-    @Override
-    void eat() {
-        System.out.println("Crow is eating");
-    }
-}
+// interface
 
 
 
-public class main {
-    public static void main(String[] args) {
-        Bird b = new sparrow();
-        b.eat();
-        b.fly();
 
-        b = new Crow();
-        b.fly();
-        b.eat();
 
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
